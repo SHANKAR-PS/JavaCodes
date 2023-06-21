6d8be4ecb8a5952846230877cafb3d484120e05c
SHANKAR-PS <136359327+SHANKAR-PS@users.noreply.github.com>
2023-06-21 21:48:15 +0530

    Datatypes
    Examples of all primitives data types in Java

---
+++ src/Datatypes.java
@@ -0,0 +1,22 @@
+public class Datatypes {
+    public static void main(String[] args) {
+        byte mByte = 84;
+        int number = 90;
+        short shortNum = 245;
+        long mLong = 24324;
+        boolean myBool = false;
+        double mDouble= 7.4324;
+        float mFloat = 324.4f;
+        char character = 'y';
+
+        System.out.println("Data types in Java :");
+        System.out.println("byte " + mByte);
+        System.out.println("integer is " +number);
+        System.out.println("short "+shortNum);
+        System.out.println("long datatype " +mLong);
+        System.out.println("double datatype " +mDouble);
+        System.out.println("float datatype " +mFloat);
+        System.out.println("char datatype " +character);
+        System.out.println("boolean value "+myBool);
+    }
+}
