1bfa27713b45a11e41df460240e520f2ae0daa40
SHANKAR-PS <136359327+SHANKAR-PS@users.noreply.github.com>
2023-07-07 22:33:44 +0530

    All Operators
    Performing all Operators in java

---
+++ src/basics/basicOperators.java
@@ -0,0 +1,26 @@
+package basics;
+
+import java.util.Scanner;
+
+public class basicOperators {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("1)Arithmetic Operator");
+        System.out.println("2)Unary Operator");
+        System.out.println("3)Assignment Operator");
+        System.out.println("4)Relational Operator");
+        System.out.println("5)Logical Operator");
+        System.out.println("6)Ternary Operator");
+        System.out.println("7)Bitwise Operator");
+        System.out.println("8)Shift Operators");
+
+
+
+
+        System.out.println("Enter the operators you want to perform :");
+
+
+
+    }
+}
