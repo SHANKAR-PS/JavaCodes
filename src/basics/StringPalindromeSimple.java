9650384edc44ece7db764e8e6585a06ab97c01ba
SHANKAR-PS <136359327+SHANKAR-PS@users.noreply.github.com>
2023-07-08 20:12:37 +0530

    Simple String Palindrome
    Used Functions
    replaceAll and reverse in StringBuilder and Equals

--- src/basics/StringPalindromeSimple.java
+++ src/basics/StringPalindromeSimple.java
@@ -10,12 +10,30 @@ public class StringPalindromeSimple {
         String s3 = s1+s2;//concatenating two strings
         System.out.println(s3);
 
-        StringBuilder sb = new StringBuilder(s3).reverse();//reverse two strings using string builder
-        //String utils also used this method
-        System.out.println(sb);//sb is a string builder form it is not a string
+        String one = s3.replaceAll("[^a-zA-Z]","");
+        //replaceAll function omits the all special characters and returns only alphabets
+        //if we want numbers put ^a-zA-z0-9 it accepts numbers also
 
-        String str = sb.toString();
+        reverse(one);
+
+
+    }
+    static void reverse(String one){
+
+
+        StringBuilder sb = new StringBuilder(one.toLowerCase()).reverse();//reverse two strings using string builder
+        //String utils also used this method
+        System.out.println("StringBuilder form "+sb);//sb is a string builder form it is not a string
+
+        String str = sb.toString();//converting string builder into string
         System.out.println(str);//printing in string format
 
+        //perform palindrome
+        if(str.equals(one)){
+            System.out.println("palindrome");
+        }
+        else {
+            System.out.println("not palindrome");
+        }
     }
 }
