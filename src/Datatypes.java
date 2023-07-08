52b929352d5ba95dd5e1684045caeee587fc2d96
SHANKAR-PS <136359327+SHANKAR-PS@users.noreply.github.com>
2023-07-08 20:16:09 +0530

    Datatypes in java
    Displays minimum and maximum value of every data types

--- src/Datatypes.java
+++ src/Datatypes.java
@@ -18,5 +18,16 @@ public class Datatypes {
         System.out.println("float datatype " +mFloat);
         System.out.println("char datatype " +character);
         System.out.println("boolean value "+myBool);
+
+
+        System.out.printf("\nByte MaxValue %d and MinValue %d",Byte.MAX_VALUE,Byte.MIN_VALUE);
+        System.out.printf("\nShort MaxValue %d and MinValue %d",Short.MAX_VALUE,Short.MIN_VALUE);
+        System.out.printf("\nInteger Maxvalue %d and MinValue %d",Integer.MAX_VALUE,Integer.MIN_VALUE);
+        System.out.printf("\nLong MaxValue %d and MinValue %d",Long.MAX_VALUE,Long.MIN_VALUE);
+        System.out.printf("\nfloat Maxvalue %f and MinValue %f",Float.MAX_VALUE,Float.MIN_VALUE);
+       //System.out.printf("\n double Maxvalue %L and MinValue %L",Double.MAX_VALUE,Double.MIN_VALUE);
+        System.out.printf("\nCharacter MaxValue %c and MinValue %c",Character.MAX_VALUE,Character.MIN_VALUE);
+        System.out.printf("\nBoolean %b",Boolean.TYPE);
+
     }
 }
